46640c0960e2750985c243c93311172f5af2f2ae
airtonlimajr <airtonlimajr@outlook.com>
2021-05-29 09:45:25 -0300

    Finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,32 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+interface ImagesData {
+  data: [
+    {
+      title: string;
+      description: string;
+      url: string;
+      ts: number;
+      id: string;
+    }
+  ];
+  after?: {
+    id: string;
+  };
+}
 
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<ImagesData> {
+    const { data } = await api.get('/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,20 +40,21 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: ({ after }) => after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(page => page.data.map(item => item)).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +62,12 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
